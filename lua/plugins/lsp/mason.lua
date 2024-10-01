5c50cf10cefd6ef363ac0d6c830134eaaadb59ad
Ayslan-gamedev <94746735+ayslan-gamedev@users.noreply.github.com>
2024-10-01 22:07:11 +0000

    nvim is ready to be used

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -4,40 +4,31 @@ return {
     -- to use lsp implementations
     "williamboman/mason-lspconfig.nvim",
 
-    -- to make instalation of packages more easy
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    -- to auto formatter
+    "stevearc/conform.nvim",
 
-    "stevearc/conform.nvim"
+    -- to linter
+    "mfussenegger/nvim-lint",
   },
-  config = function ()
-
+  config = function()
     -- stup mason
     require("mason").setup({
-      ui = { icons = {
-        package_installed = "",
-        package_pending = "",
-        package_uninstalled = ""
-      }}
-    })
-
-    -- install tools
-    require("mason-tool-installer").setup({
-      ensure_installed = {
-        "clang-format",   -- formatter for c/c++
-        "stylua",          -- formatter for lua
-        "cpplint",
---        "cppcheck",
---        "lualint",
---        "luacheck"
+      ui = {
+        icons = {
+          package_installed = "",
+          package_pending = "",
+          package_uninstalled = ""
+        }
       }
     })
 
+    -- setup conform
     require("conform").setup({
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_format = "fallback",
-  },
-})
+      format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_format = "fallback",
+      },
+    })
   end
 }
