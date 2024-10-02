5b801a6f3d5b71c37019e686767d8fb76d6169c8
Ayslan-gamedev <94746735+ayslan-gamedev@users.noreply.github.com>
2024-10-02 17:58:20 +0000

    Better html coding

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -1,11 +1,11 @@
-  -- LSP CONFIG
+-- LSP CONFIG
 return {
   "neovim/nvim-lspconfig",
   dependencies = {
     -- to mason implementations
     "williamboman/mason-lspconfig.nvim"
   },
-  config = function ()
+  config = function()
     local lspconfig = require('lspconfig')
 
     local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -20,19 +20,27 @@ return {
     -- initialize lsp configuration with mason
     require('mason-lspconfig').setup_handlers({
       -- Default handler for all installed servers
-      function (server_name)
+      function(server_name)
         setup_server(server_name)
       end,
 
       -- Specific servers with the same setup function
-      ["lua_ls"] = function ()
+      ["lua_ls"] = function()
         setup_server("lua_ls")
       end,
 
-      ["clangd"] = function ()
+      ["clangd"] = function()
         setup_server("clangd")
-      end
+      end,
+
+      ["emmet_ls"] = function()
+        -- configure emmet language server
+        lspconfig["emmet_ls"].setup({
+          capabilities = capabilities,
+          filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
+        })
+      end,
+
     })
   end
 }
-
--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -24,6 +24,9 @@ return {
 
     -- setup conform
     require("conform").setup({
+      formatters_by_ft = {
+        html = { "prettier" }
+      },
       format_on_save = {
         -- These options will be passed to conform.format()
         timeout_ms = 500,
