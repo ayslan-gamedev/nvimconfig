f57f99a3b1e63e302f4bb800c2ccb422c142379b
Ayslan-gamedev <94746735+ayslan-gamedev@users.noreply.github.com>
2024-10-07 18:40:51 +0000

    fix folder tree

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,63 +1,62 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "stevearc/conform.nvim",
-    "mfussenegger/nvim-lint",
-    "williamboman/mason-lspconfig.nvim",
-  },
-  config = function()
-    -- Setup mason
-    require("mason").setup({
-      ui = {
-        icons = {
-          package_installed = "",
-          package_pending = "",
-          package_uninstalled = ""
-        }
-      }
-    })
+	"williamboman/mason.nvim",
+	dependencies = {
+		"stevearc/conform.nvim",
+		"mfussenegger/nvim-lint",
+		"williamboman/mason-lspconfig.nvim",
+	},
+	config = function()
+		-- Setup mason
+		require("mason").setup({
+			ui = {
+				icons = {
+					package_installed = "",
+					package_pending = "",
+					package_uninstalled = "",
+				},
+			},
+		})
 
-    -- Setup conform
-    require("conform").setup({
-      formatters_by_ft = {
-        html = { "prettier" },
-        css = { "prettier" },
-        lua = { "stylua" },
-        python = { "black", "isort" },
-        javascript = { "eslint_d" },
-        c = { "clang-format" },   -- Adicionado para C
-        cpp = { "clang-format" }, -- Adicionado para C++
-      },
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_format = "fallback",
-      },
-    })
+		-- Setup conform
+		require("conform").setup({
+			formatters_by_ft = {
+				html = { "prettier" },
+				css = { "prettier" },
+				lua = { "stylua" },
+				python = { "black", "isort" },
+				javascript = { "eslint_d" },
+				c = { "clang-format" },
+				cpp = { "clang-format" },
+			},
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			},
+		})
 
-    -- Setup nvim-lint
-    require("lint").linters_by_ft = {
-      lua = { "luacheck" },
-      python = { "flake8", "pylint" },
-      javascript = { "eslint" },
-      c = { "clang-tidy" },   -- Adicionado para C
-      cpp = { "clang-tidy" }, -- Adicionado para C++
-    }
+		-- Setup nvim-lint
+		require("lint").linters_by_ft = {
+			lua = { "luacheck" },
+			python = { "flake8", "pylint" },
+			javascript = { "eslint" },
+			c = { "clang-tidy" },
+			cpp = { "clang-tidy" },
+		}
 
-    require("mason-lspconfig").setup({
-      -- list of servers for mason to install
-      ensure_installed = {
-        "tsserver",
-        "html",
-        "cssls",
-        "tailwindcss",
-        "svelte",
-        "lua_ls",
-        "graphql",
-        "emmet_ls",
-        "prismals",
-        "pyright",
-        "clangd",
-      },
-    })
-  end,
+		require("mason-lspconfig").setup({
+			-- list of servers for mason to install
+			ensure_installed = {
+				"html",
+				"cssls",
+				"tailwindcss",
+				"svelte",
+				"lua_ls",
+				"graphql",
+				"emmet_ls",
+				"prismals",
+				"pyright",
+				"clangd",
+			},
+		})
+	end,
 }
