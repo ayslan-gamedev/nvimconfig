1372f5d3dda69af1fa5367004575b946a8d7d427
Ayslan-gamedev <94746735+ayslan-gamedev@users.noreply.github.com>
2024-10-09 23:22:07 +0000

    JS support and colorizer

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -1,92 +1,98 @@
 return {
-	"neovim/nvim-lspconfig",
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-		{ "antosha417/nvim-lsp-file-operations", config = true },
-		{ "folke/neodev.nvim", opts = {} },
-	},
-	config = function()
-		-- import lspconfig plugin
-		local lspconfig = require("lspconfig")
+  "neovim/nvim-lspconfig",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    { "antosha417/nvim-lsp-file-operations", config = true },
+    { "folke/neodev.nvim",                   opts = {} },
+  },
+  config = function()
+    -- import lspconfig plugin
+    local lspconfig = require("lspconfig")
 
-		-- used to enable autocompletion (assign to every lsp server config)
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    -- used to enable autocompletion (assign to every lsp server config)
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-		-- mason config
-		require("mason-lspconfig").setup_handlers({
-			-- default handler for installed servers
-			function(server_name)
-				lspconfig[server_name].setup({
-					capabilities = capabilities,
-				})
-			end,
-			["svelte"] = function()
-				-- configure svelte server
-				lspconfig["svelte"].setup({
-					capabilities = capabilities,
-					on_attach = function(client, bufnr)
-						vim.api.nvim_create_autocmd("BufWritePost", {
-							pattern = { "*.js", "*.ts" },
-							callback = function(ctx)
-								client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
-							end,
-						})
-					end,
-				})
-			end,
-			["graphql"] = function()
-				-- configure graphql language server
-				lspconfig["graphql"].setup({
-					capabilities = capabilities,
-					filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
-				})
-			end,
-			["emmet_ls"] = function()
-				-- configure emmet language server
-				lspconfig["emmet_ls"].setup({
-					capabilities = capabilities,
-					filetypes = {
-						"html",
-						"typescriptreact",
-						"javascriptreact",
-						"css",
-						"sass",
-						"scss",
-						"less",
-						"svelte",
-					},
-				})
-			end,
-			["lua_ls"] = function()
-				-- configure lua server (with special settings)
-				lspconfig["lua_ls"].setup({
-					capabilities = capabilities,
-					settings = {
-						Lua = {
-							-- make the language server recognize "vim" global
-							diagnostics = {
-								globals = { "vim" },
-							},
-							completion = {
-								callSnippet = "Replace",
-							},
-						},
-					},
-				})
-			end,
-			["clangd"] = function()
-				-- configure clangd language server
-				lspconfig["clangd"].setup({
-					capabilities = capabilities,
-					cmd = { "clangd", "--background-index" }, -- Adiciona o comando e as opções do clangd
-					filetypes = { "c", "cpp", "objc", "objcpp" },
-					on_attach = function(client, bufnr)
-						-- Opcional: configurações adicionais no attach
-						-- Exemplo: definir mapeamentos de teclas aqui, se necessário
-					end,
-				})
-			end,
-		})
-	end,
+    -- mason config
+    require("mason-lspconfig").setup_handlers({
+      -- default handler for installed servers
+      function(server_name)
+        lspconfig[server_name].setup({
+          capabilities = capabilities,
+        })
+      end,
+      ["svelte"] = function()
+        -- configure svelte server
+        lspconfig["svelte"].setup({
+          capabilities = capabilities,
+          on_attach = function(client, bufnr)
+            vim.api.nvim_create_autocmd("BufWritePost", {
+              pattern = { "*.js", "*.ts" },
+              callback = function(ctx)
+                client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
+              end,
+            })
+          end,
+        })
+      end,
+      ["graphql"] = function()
+        -- configure graphql language server
+        lspconfig["graphql"].setup({
+          capabilities = capabilities,
+          filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
+        })
+      end,
+      ["emmet_ls"] = function()
+        -- configure emmet language server
+        lspconfig["emmet_ls"].setup({
+          capabilities = capabilities,
+          filetypes = {
+            "html",
+            "typescriptreact",
+            "javascriptreact",
+            "css",
+            "sass",
+            "scss",
+            "less",
+            "svelte",
+          },
+        })
+      end,
+      ["lua_ls"] = function()
+        -- configure lua server (with special settings)
+        lspconfig["lua_ls"].setup({
+          capabilities = capabilities,
+          settings = {
+            Lua = {
+              -- make the language server recognize "vim" global
+              diagnostics = {
+                globals = { "vim" },
+              },
+              completion = {
+                callSnippet = "Replace",
+              },
+            },
+          },
+        })
+      end,
+      ["clangd"] = function()
+        -- configure clangd language server
+        lspconfig["clangd"].setup({
+          capabilities = capabilities,
+          cmd = { "clangd", "--background-index" }, -- Adiciona o comando e as opções do clangd
+          filetypes = { "c", "cpp", "objc", "objcpp" },
+          on_attach = function(client, bufnr)
+            -- Opcional: configurações adicionais no attach
+            -- Exemplo: definir mapeamentos de teclas aqui, se necessário
+          end,
+        })
+      end,
+      ["tsserver"] = function()
+        lspconfig["tsserver"].setup({
+          capabilities = capabilities,
+        })
+      end,
+
+    })
+  end,
 }
--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,62 +1,63 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
-		"stevearc/conform.nvim",
-		"mfussenegger/nvim-lint",
-		"williamboman/mason-lspconfig.nvim",
-	},
-	config = function()
-		-- Setup mason
-		require("mason").setup({
-			ui = {
-				icons = {
-					package_installed = "",
-					package_pending = "",
-					package_uninstalled = "",
-				},
-			},
-		})
+  "williamboman/mason.nvim",
+  dependencies = {
+    "stevearc/conform.nvim",
+    "mfussenegger/nvim-lint",
+    "williamboman/mason-lspconfig.nvim",
+  },
+  config = function()
+    -- Setup mason
+    require("mason").setup({
+      ui = {
+        icons = {
+          package_installed = "",
+          package_pending = "",
+          package_uninstalled = "",
+        },
+      },
+    })
 
-		-- Setup conform
-		require("conform").setup({
-			formatters_by_ft = {
-				html = { "prettier" },
-				css = { "prettier" },
-				lua = { "stylua" },
-				python = { "black", "isort" },
-				javascript = { "eslint_d" },
-				c = { "clang-format" },
-				cpp = { "clang-format" },
-			},
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_format = "fallback",
-			},
-		})
+    -- Setup conform
+    require("conform").setup({
+      formatters_by_ft = {
+        html = { "prettier" },
+        css = { "prettier" },
+        lua = { "stylua" },
+        python = { "black", "isort" },
+        javascript = { "eslint_d" },
+        c = { "clang-format" },
+        cpp = { "clang-format" },
+      },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_format = "fallback",
+      },
+    })
 
-		-- Setup nvim-lint
-		require("lint").linters_by_ft = {
-			lua = { "luacheck" },
-			python = { "flake8", "pylint" },
-			javascript = { "eslint" },
-			c = { "clang-tidy" },
-			cpp = { "clang-tidy" },
-		}
+    -- Setup nvim-lint
+    require("lint").linters_by_ft = {
+      lua = { "luacheck" },
+      python = { "flake8", "pylint" },
+      javascript = { "eslint" },
+      c = { "clang-tidy" },
+      cpp = { "clang-tidy" },
+    }
 
-		require("mason-lspconfig").setup({
-			-- list of servers for mason to install
-			ensure_installed = {
-				"html",
-				"cssls",
-				"tailwindcss",
-				"svelte",
-				"lua_ls",
-				"graphql",
-				"emmet_ls",
-				"prismals",
-				"pyright",
-				"clangd",
-			},
-		})
-	end,
+    require("mason-lspconfig").setup({
+      -- list of servers for mason to install
+      ensure_installed = {
+        "html",
+        "cssls",
+        "tailwindcss",
+        "svelte",
+        "lua_ls",
+        "graphql",
+        "emmet_ls",
+        "prismals",
+        "pyright",
+        "clangd",
+        "ts_ls"
+      },
+    })
+  end,
 }
